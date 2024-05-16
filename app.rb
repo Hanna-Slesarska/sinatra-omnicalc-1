58eada3bc8ac3550951c2d5b62b15a23f02f2286
Hanna Slesarska <127249050+Hanna-Slesarska@users.noreply.github.com>
2024-05-16 18:53:19 +0000

    done!

--- app.rb
+++ app.rb
@@ -10,6 +10,10 @@ get("/square_root/new") do
   erb(:new_square_root)
 end
 
+get("/payment/new") do
+  erb(:new_payment)
+end
+
 get("/square/results") do
   @the_num = params.fetch("users_number").to_f
   @the_result = @the_num ** 2
@@ -24,9 +28,35 @@ get("/square_root/results") do
   erb(:root_results)
 end
 
+get("/payment/results") do
+
+  @the_apr = (params.fetch("user_apr")).to_f
+  @rate = @the_apr.to_fs(:percentage, { :precision => 4 } ) 
+  @the_time = (params.fetch("user_years")).to_i
+  @pv = (params.fetch("user_pv")).to_f
+  @principal = @pv.to_fs(:currency) 
+  @pv = @pv.round(2)
+
+  @rate_i = @the_apr /100
+  @month = 12
+  @p = (1-(1+@rate_i/@month)**(-@month*@the_time))/(@rate_i/@month)
+  @payment = (@pv/@p).round(2)
+  @payment = @payment.to_fs(:currency)
+  erb(:payment_results)
+end
+
+get("/random/new") do
+  erb(:random_new)
+end
+
+get("/random/results") do
+  @first_one = (params.fetch("user_min")).to_f
+  @second_one = (params.fetch("user_max")).to_f
+  @result = rand(@first_one..@second_one)
+
+erb(:random_result)
+end
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
